746afa24f66f7fdf2af334cc1237a02d7a52f829
JuanPabloVelozo <jpvelozo219@gmail.com>
2024-05-05 23:05:26 -0400

    despliegue de informacion basica del detalle

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { Pokemon } from '../../interfaces/pokemon';
+import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.component.html',
   styleUrls: ['./detalle.component.scss']
 })
-export class DetalleComponent {
-
+export class DetalleComponent  {
+  @Input() pokemon?: Pokemon;
+  descripcion: string = "";
+  constructor(private pokemonService: PokemonService) { }
+  ngOnChanges(): void {
+    if (this.pokemon) {
+      this.pokemonService.getByDescripcion(this.pokemon?.id).then(res => {
+        this.descripcion = res;
+      })
+    }
+  }
+  
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -10,13 +10,13 @@ export class PokemonService {
 
   constructor() { }
 
-  // Observable para la variable terminoBusqueda
+  // recuperacion para la variable terminoBusqueda
   get terminoBusqueda$() {
     return this._terminoBusqueda.asObservable();
   }
 
   async getByPage(): Promise<Resultado[]> {
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=151');
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=151`);
     const resJson = await res.json();
     if (resJson.results.length > 0) return resJson.results;
     return [];
@@ -28,7 +28,11 @@ export class PokemonService {
     return resJson;
   }
 
-  getByDescripcion() {
+  async getByDescripcion(id: string | number): Promise<string> {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
+    const resJson = await res.json();
+    const texto = resJson.flavor_text_entries.find((entry: any) => entry.language.name === 'es');
+    return texto.flavor_text;
   }
 
   almacenarBusqueda(termino: string): void {
